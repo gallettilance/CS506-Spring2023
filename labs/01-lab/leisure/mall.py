d4096edd3827af028083bb1bfd90201359fe494b
Haniel <hainel20008@gmail.com>
2023-01-31 17:34:03 -0500

    Created mall

--- labs/01-lab/leisure/mall.py
+++ labs/01-lab/leisure/mall.py
@@ -1,3 +1,12 @@
 def draw_mall():
-    print("mall not found")
-    return
+   print(" __________")
+   print("|          |")
+   print("|  MALL    |")
+   print("|__________|")
+   print("|    |     |")
+   print("|    |     |")
+   print("|____|_____|")
+   print("|    |     |")
+   print("|    |     |")
+   print("|____|_____|")
+   return
