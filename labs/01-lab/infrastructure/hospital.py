6c64873cfc9cb8f5e8e54e6eece1f6fd82ee38c8
CoToYo <56789038+CoToYo@users.noreply.github.com>
2023-01-31 12:06:16 -0500

    new hospital

--- labs/01-lab/infrastructure/hospital.py
+++ labs/01-lab/infrastructure/hospital.py
@@ -1,3 +1,13 @@
 def draw_hospital():
-    print("hospital not found")
-    return
+    print("[Hospital]:")
+    
+    print(" "*13, "|")
+    print(" "*10, "-"*7)
+    print(" "*13, "|")
+    print("-"*29)
+    for i in range(15):
+        print("|", " "*5, "[ ]", " "*5,"[ ]"," "*5,"|")
+        
+    print("|", " "*9, "_____", " "*9, "|")
+    for i in range (3):
+        print("|", " "*9, "|   |", " "*9, "|")
