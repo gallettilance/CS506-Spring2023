49e38d94b61a4a0da5632e824d932a251dcfe1b2
Specter43 <zheyuyang43@gmail.com>
2023-01-31 16:08:45 -0500

    Add to draw_road, now it can draw a tree!

--- labs/01-lab/infrastructure/road.py
+++ labs/01-lab/infrastructure/road.py
@@ -1,3 +1,13 @@
 def draw_road():
-    print("road not found")
+    def draw_tree(height):
+        for i in range(1, height + 1):
+            spaces = height - i
+            stars = 2 * i - 1
+            print(" " * spaces + "*" * stars)
+        trunk_height = height // 4
+        if trunk_height < 3:
+            trunk_height = 3
+        spaces = height - trunk_height
+        print(" " * spaces + "####" * (trunk_height // 4))
+    draw_tree()
     return
