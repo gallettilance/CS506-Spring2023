30ca6f22f091f97ff00597ee6ea680da34eebc65
Yiran-H <tina721199@126.com>
2023-01-31 10:55:32 -0500

    change library.py

--- labs/01-lab/education/library.py
+++ labs/01-lab/education/library.py
@@ -1,6 +1,7 @@
 def draw_library():
     print("library not found")
     print("you need to build a library")
-    print("-------")
-    print("|      |")
+    print("--------")
+    print("|       |")
+    print("--------")
     return
