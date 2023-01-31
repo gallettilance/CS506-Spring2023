af2ec3ca6c3d356702c729bc2c7c317bd58ad0e8
bbbBao <tianyib831@gmail.com>
2023-01-31 14:51:54 -0500

    draw library and school

--- labs/01-lab/education/library.py
+++ labs/01-lab/education/library.py
@@ -1,3 +1,10 @@
 def draw_library():
-    print("library not found")
+    print("library")
+    print("         +")
+    print("         A")
+    print("      __/_\__")
+    print("     /\-'o'-/\ ")
+    print("    _||:<_>:||_ ")
+    print("   /\_/=====\_/\ ")
+    print("   _|:_:_[I]_:_:| ")
     return
--- labs/01-lab/education/school.py
+++ labs/01-lab/education/school.py
@@ -1,3 +1,10 @@
 def draw_school():
-    print("school not found")
+    print("school")
+    print("                .-'`-._ _ ")
+    print("            ;.'________'.; ")
+    print(" _________n.[____________].n_________")
+    print("|===========||..||..||..||===========|")
+    print("|LI LI LI LI||LI||LI||LI||LI LI LI LI|")
+    print("|.. .. .. ..||..||..||..||.. .. .. ..|")
+    print("|LI LI LI LI||LI||LI||LI||LI LI LI LI|")
     return
