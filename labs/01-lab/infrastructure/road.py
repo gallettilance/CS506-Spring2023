7548ea4617e9699944f598db8df960344688d833
Baicheng Fang <baichengfang@Baichengs-MacBook-Air.local>
2023-01-31 20:45:33 -0500

    Add Commonwealth Ave

--- labs/01-lab/infrastructure/road.py
+++ labs/01-lab/infrastructure/road.py
@@ -1,6 +1,12 @@
 def draw_road():
     print("road not found")
-    print("_"*30, "\n")
-    print("-----Commonwealth Avenue-----")
-    print("_"*30)
+    print("_"*40, "\n")
+    print("- "*20)
+    print("_"*40)
+    print("-"*40)
+    print(" "*9, "Commonwealth Avenue", " "*10)
+    print("-"*40)
+    print("_"*40, "\n")
+    print("- "*20)
+    print("_"*40)
     return
