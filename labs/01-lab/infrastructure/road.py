2e3a05de2b5e06adab4a4ce4d9b5b2e3c2a37047
David Sullo <dsullo@bu.edu>
2023-02-01 09:13:27 -0500

    Added string representing road to be printed by draw_road()

--- labs/01-lab/infrastructure/road.py
+++ labs/01-lab/infrastructure/road.py
@@ -1,3 +1,12 @@
 def draw_road():
-    print("road not found")
+    # found here: https://www.asciiart.eu/buildings-and-places/other
+    road = """
+    ___  ,--.  __________________________/   ,   /_______
+        'O---O'~
+    _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _   ,--.   _ _ _ _ _
+            _____                       ~'O---O'
+    _______< CS506|____        __________________________
+            ||      /   ,   /
+    """
+    print(road)
     return
